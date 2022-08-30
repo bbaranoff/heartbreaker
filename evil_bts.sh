d677e3ef3145f7ffed0a31f9562bfb0054204d62
bastien baranoff <37385191+bbaranoff@users.noreply.github.com>
2022-08-30 23:02:36 +0200

    Update evil_bts.sh

--- evil_bts.sh
+++ evil_bts.sh
@@ -10,7 +10,7 @@ tmux send-keys -t "softsim" '/softsim/src/demo_server.rb --type pcsc --socket un
 tmux send-keys -t "trxs" '/heartbreaker/osmocom-bb/src/target/trx_toolkit/fake_trx.py' Enter
 sleep 1
 tmux send-keys -t "Evil-nitb" '/heartbreaker/nitb.sh' Enter
-tmux send-keys -t "Evil-bts" '/heartbreaker/osmo-bts/src/osmo-bts-trx/osmo-bts-trx -c /heartbreaker/evil-bts.cfg' Enter
+tmux send-keys -t "Evil-bts" 'osmo-bts-trx -c /heartbreaker/evil-bts.cfg' Enter
 tmux send-keys -t "Orange-trxcon" '/heartbreaker/osmocom-bb/src/host/trxcon/trxcon' Enter
 sleep 1
 tmux send-keys -t "Orange-MS" '/heartbreaker/osmocom-bb/src/host/layer23/src/mobile/mobile' Enter
